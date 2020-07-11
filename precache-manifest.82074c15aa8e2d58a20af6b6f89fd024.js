979bc64427141070cd122c03397383d750ebd81e
R P Buvan Varun <buvanvarun.acad@gmail.com>
2020-07-11 20:46:50 +0530

    Updates

---
+++ precache-manifest.82074c15aa8e2d58a20af6b6f89fd024.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd1bb7d815323f5fafff106582a420f",
+    "url": "/utonEnergia/index.html"
+  },
+  {
+    "revision": "1e40adb041e6aa928148",
+    "url": "/utonEnergia/static/css/main.6a09f567.chunk.css"
+  },
+  {
+    "revision": "50b006d44b3db39cd64f",
+    "url": "/utonEnergia/static/js/2.b988a260.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/utonEnergia/static/js/2.b988a260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e40adb041e6aa928148",
+    "url": "/utonEnergia/static/js/main.d933e6d6.chunk.js"
+  },
+  {
+    "revision": "95c1cf6a6c416af0f2b0",
+    "url": "/utonEnergia/static/js/runtime-main.9c46f5e7.js"
+  },
+  {
+    "revision": "d68117eef005d9be54af69418db1225e",
+    "url": "/utonEnergia/static/media/vehicle-dark.d68117ee.png"
+  },
+  {
+    "revision": "d68117eef005d9be54af69418db1225e",
+    "url": "/utonEnergia/static/media/vehicle.d68117ee.png"
+  }
+]);
